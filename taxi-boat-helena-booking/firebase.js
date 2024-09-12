c234b0550bae04f93b20f1e8cb124b88d0b89178
Marko <mdamijanj@student.unipu.hr>
2024-09-12 07:08:01 +0200

    Prvi commit

---
+++ taxi-boat-helena-booking/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAVKEk6Tz3nhDEpwNTwN6Qd5aDOct2FSGM",
+  authDomain: "taxi-boat-helena-booking.firebaseapp.com",
+  projectId: "taxi-boat-helena-booking",
+  storageBucket: "taxi-boat-helena-booking.appspot.com",
+  messagingSenderId: "476887855675",
+  appId: "1:476887855675:web:c220300d9ff8baa1da221e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
